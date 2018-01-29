57320da0f64ee6286fa2bbf011406f8a160da12d
abelardogg <abelardogy@gmail.com>
2018-01-28 21:53:52 -0700

    get header from server

--- app.js
+++ app.js
@@ -8,14 +8,19 @@ app.get('/', (req, res) =>{
     res.sendFile(__dirname + '/views/home.html');
 });
 
+// templates
 app.get('/user-info', function (req, res) {
     res.send({userName:'Abelardo'})
 });
 
+app.post('/main-header', function (req, res) {
+    res.sendFile(__dirname + '/views/templates/header.html');
+});
 
 // 404
 app.use(function (req, res, next) {
     res.status(404).sendFile(__dirname + '/views/404.html');
-  });
+});
 
+// START SERVER
 app.listen(3000, ()=> console.log('server up on port 3000'));
--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -1,7 +1,30 @@
 $(document).ready(function () {
+
+    getUserInfo();
+    getHeader();
+});
+
+/**
+ * GET user main info (Name, last name, city, etc.).
+ */
+function getUserInfo(){
     const $xprssUserName = $('.xprss-user-name');
     $.get( "/user-info", function( data ) {
         $xprssUserName.append(data.userName);
         console.log(data);
     });
-});
+}
+
+function hrefHandler(){
+    $('.redirect-home').attr('href','/')
+}
+
+function getHeader() {
+    $.post( "/main-header", function( data ) {
+        $('#main-header').append(data);
+        console.log(data);
+        hrefHandler();
+
+    });
+
+}
