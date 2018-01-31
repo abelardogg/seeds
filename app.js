08d376905f3664d701b9302ecaaa7b14e7b77d88
abelardogg <abelardogy@gmail.com>
2018-01-30 19:12:09 -0700

    added ejs engine

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
 const express = require('express');
 const app = express();
 
+// EJS engine
+app.set('view engine', 'ejs');
+
 app.use(express.static('public'));
 
 // HOMEPAGE
 app.get('/', (req, res) =>{
-    res.sendFile(__dirname + '/views/home.html');
+    res.render('pages/home');
 });
 
 // PROFILE
 app.get('/profile', (req, res) =>{
-    res.sendFile(__dirname + '/views/profile.html');
+    res.render('pages/profile');
 });
 
-// DATA
-app.post('/user-info', function (req, res) {
-    res.sendFile(__dirname + '/test/json/test-user.json');
+app.get('/yards', (req, res) =>{
+    res.render('pages/yards');
 });
 
-// templates
-app.post('/main-header', function (req, res) {
-    res.sendFile(__dirname + '/views/templates/header.html');
-});
+//
+// // DATA Test
+// app.post('/user-info', function (req, res) {
+//     res.sendFile(__dirname + '/test/json/test-user.json');
+// });
+//
+// // templates
+// app.post('/main-header', function (req, res) {
+//     res.sendFile(__dirname + '/views/templates/header.html');
+// });
 
 // 404
 app.use(function (req, res, next) {
-    res.status(404).sendFile(__dirname + '/views/404.html');
+    res.status(404).render('pages/404');
 });
 
 // START SERVER
